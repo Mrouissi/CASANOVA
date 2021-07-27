0a27df8e9157e70bbcf3eed6cc8920f9f0385744
hassogba <thiking12@gmail.com>
2021-07-27 18:36:24 +0200

    data from bdd

--- src/app/client-commande/client-commande.component.ts
+++ src/app/client-commande/client-commande.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ClientCommandeService } from './client-commande.service';
 
 @Component({
   selector: 'app-client-commande',
@@ -7,18 +10,132 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientCommandeComponent implements OnInit {
   select: any;
+  agences =  [{'id': 0, 'nom': "" }];
+  dataSource = new MatTableDataSource(this.agences);
+
+  entiteVente=  [{'id': 0, 'nom': "" }];
+  entiteVenteSource = new MatTableDataSource(this.entiteVente);
+
+  contactOrigines=[{'id': 0, 'nom': "" }];
+  contactOriginesSource=new MatTableDataSource(this.contactOrigines);
+
+  typeTravaux=[{'id': 0, 'nom': "","elementARenoverList":[]}];
+  typeTravauxSource=new MatTableDataSource(this.typeTravaux);
+  
+  elements = [{'id': 0, 'nom': "","prestationARealiserList":[]}]
+  elementsSource = new MatTableDataSource(this.elements);
+
+  prestations = [{'id': 0, 'nom': "","supportExistantList":[]}]
+  prestationsSource = new MatTableDataSource(this.prestations);
+
+  supports= [{'id': 0, 'nom': "" }];
+  supportSource = new MatTableDataSource(this.supports);
 
   reglementMode = [
     {value : 'Comptant', name : 'Comptant'},
     {value : 'Financement', name : 'Financement'}] ;
 
-  constructor() { }
+  constructor(private router: Router, private service : ClientCommandeService) { }
 
   ngOnInit(): void {
+
+    this.service.getAllAgence().subscribe(data => {
+
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.agences = res
+      const row = this.dataSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.dataSource.data = row;
+      }
+    })
+
+    this.service.getAllEntiteVente().subscribe(data => {
+      
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.entiteVente = res
+
+      const row1 = this.entiteVenteSource.data;
+      row1.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row1.push(res[i]);
+        this.entiteVenteSource.data = row1;
+      }
+    });
+
+
+
+    this.service.getAllOrigineContact().subscribe(data => {
+     
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.contactOrigines = res
+      const row = this.contactOriginesSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.contactOriginesSource.data = row;
+      }
+    })
+
+    this.service.getAllTypesTravaux().subscribe(data => {
+     
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.typeTravaux = res
+      const row = this.typeTravauxSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.typeTravauxSource.data = row;
+      }
+    })
+
+    this.service.getAllElementARenover().subscribe(data => {
+      
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.elements = res
+      const row = this.elementsSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.elementsSource.data = row;
+      }
+    })
+
+    this.service.getAllPrestations().subscribe(data => {
+    
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.prestations = res
+      const row = this.prestationsSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.prestationsSource.data = row;
+      }
+    })
+    this.service.getAllSupports().subscribe(data => {
+     
+      let datas = JSON.stringify(data)
+      let res = JSON.parse(datas)
+      this.supports = res
+      const row = this.supportSource.data;
+      row.splice(0,1)
+      for(let i=0 ; i < res.length ; i++){
+        row.push(res[i]);
+        this.supportSource.data = row;
+      }
+    })
+
   }
-  onItemChange(selectedValue: string){
-    return this.select = selectedValue;
-    console.log('selected value= ' + this.select);
+
+   onItemChange(selectedValue: any){
+    return this.select = selectedValue.target.value;
   }
 
 }
---
+++ src/app/client-commande/client-commande.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientCommandeService {
+
+  baseUrl: string = environment.backend.baseURL;
+
+  headers = new HttpHeaders({
+    'content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+   })
+   headersE = new HttpHeaders({
+    'content-type': 'application/octet-stream',
+    'Access-Control-Allow-Origin': '*'
+   })
+  constructor(private httpClient: HttpClient) {}
+
+  public getAllAgence(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/agences",
+      { headers: this.headers })
+  }
+
+  public getAllEntiteVente(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/entiteVente",
+      { headers: this.headers })
+  }
+  
+  public getAllOrigineContact(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/originecontact",
+      { headers: this.headers })
+  }
+  
+  public getAllTypesTravaux(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/typestravaux",
+      { headers: this.headers })
+  }
+  public getAllElementARenover(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/elementrenover",
+      { headers: this.headers })
+  }
+
+  public getAllPrestations(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/prestationrealiser",
+      { headers: this.headers })
+  }
+  public getAllSupports(){
+    return this.httpClient.get(this.baseUrl+
+      "/api/supportexistant",
+      { headers: this.headers })
+  }
+}
